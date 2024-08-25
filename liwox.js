b87f3fa91143cd35ff25ec45801bb07f354cfcf0
Starbuckle <74896363+Starbuckle@users.noreply.github.com>
2024-08-25 17:00:03 +0800

    Update liwox.js

--- liwox.js
+++ liwox.js
@@ -17,11 +17,11 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/www.liwoxiao.com\/api\/game\/account\/data\/download url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/yx/liwoxiao.js
+^http[s]?:\/\/www.liwox.com\/api\/game\/account\/data\/download url script-response-body https://raw.githubusercontent.com/Starbuckle/quantu/main/liwox.js
 
 [mitm] 
 
-hostname = www.liwoxiao.com
+hostname = www.liwox.com
 
 *******************************/
 
