434510e1be81c755f5ca484e902a2fc7d9067e63
Starbuckle <74896363+Starbuckle@users.noreply.github.com>
2024-08-25 17:14:34 +0800

    Update liwox.js

--- liwox.js
+++ liwox.js
@@ -28,5 +28,5 @@ hostname = www.liwoxiao.com
 var body = $response.body.replace(/playerInfo_gold\\":\d+/g,'playerInfo_gold\\":88878')
 .replace(/playerInfo_buildScore\\":\d+/g,'playerInfo_buildScore\\":88998')
 .replace(/playerInfo_shipuScore\\":\d+/g,'playerInfo_shipuScore\\":88898')
-.replace(/playerInfo_diamond\\":\d+/g,'playerInfo_diamond\\":888888')
+.replace(/playerInfo_diamond\\":\d+/g,'playerInfo_diamond\\":8888')
 $done({ body });
